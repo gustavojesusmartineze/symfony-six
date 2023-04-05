b12ff8c10824ed0392b62f91f2f6320d1ae76474
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-05 19:26:25 -0400

    feat(comment): use entityManager to get all comments

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,21 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class PageController extends AbstractController
 {
     #[Route('/home')]
-    public function home(Request $request) : Response
+    public function home(EntityManagerInterface $entityManager) : Response
     {
-        $search = $request->get('search');
-        // dd($search);
+        // $entityManager->getRepository(Comment::class) gets App\Repository\CommentRepository class
+        $comments = $entityManager->getRepository(Comment::class)->findBy([], [
+            'id' => 'DESC'
+        ]);
 
-        // return new Response('Welcome, home page ' . $search);
-        return $this->render('home.html.twig', ['search' => $search]);
+        // dd($comments);
+
+        return $this->render('home.html.twig', [
+            // 'comments' => $entityManager->getRepository(Comment::class)->findAll()
+            'comments' => $comments
+        ]);
     }
 }
