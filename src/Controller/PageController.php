5bd7abccecc0810f7100d57cc3753d3434d34e66
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-05 17:52:29 -0400

    feat(home): add home page view

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class PageController
+class PageController extends AbstractController
 {
     #[Route('/home')]
     public function home(Request $request) : Response
     {
         $search = $request->get('search');
 
-        return new Response('Welcome, home page ' . $search);
+        // return new Response('Welcome, home page ' . $search);
+        return $this->render('home.html.twig', ['search' => $search]);
     }
 }
