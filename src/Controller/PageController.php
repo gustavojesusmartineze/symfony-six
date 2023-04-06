22e45f6e74c659d042005b28c51efd1bb70a3eed
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-06 07:15:39 -0400

    feat(comment): add CommentType form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,18 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
+
 use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class PageController extends AbstractController
 {
-    #[Route('/home')]
-    public function home(EntityManagerInterface $entityManager) : Response
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $entityManager, Request $request) : Response
     {
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted())
+        {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
         // $entityManager->getRepository(Comment::class) gets App\Repository\CommentRepository class
         $comments = $entityManager->getRepository(Comment::class)->findBy([], [
             'id' => 'DESC'
@@ -24,7 +37,8 @@ class PageController extends AbstractController
 
         return $this->render('home.html.twig', [
             // 'comments' => $entityManager->getRepository(Comment::class)->findAll()
-            'comments' => $comments
+            'comments' => $comments,
+            'form' => $form->createView()
         ]);
     }
 }
