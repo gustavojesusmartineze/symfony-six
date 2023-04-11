eae453457dca4e7f4a8dd54a0af29729d53dc9e9
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-11 11:33:53 -0400

    feat(post): add category entity and ManyToOne relation

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -15,6 +16,25 @@ class PostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    // 'PHP' => 'php',
+                    // 'Laravel' => 'laravel',
+                    // 'Symfony' => 'symfony'
+                    'Languages' => [
+                        'PHP' => 'php',
+                        'Javascript' => 'javascript',
+                    ],
+                    'Frameworks' => [
+                        'Laravel' => 'laravel',
+                        'Symfony' => 'symfony',
+                        'React' => 'react',
+                        'Angular' => 'angular'
+                    ]
+                ],
+                'placeholder' => 'Select one...',
+                'label' => 'Categories'
+            ])
             ->add('title', TextType::class, [
                 'label' => 'Post Title',
                 'help' => 'Think about how SEO will find you'
