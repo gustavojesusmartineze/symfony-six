8774f6fc8606960a25826a4536e2f6fd712df22d
Gustavo Martínez <gustavomartinez1990@gmail.com>
2023-04-11 12:45:18 -0400

    feat(post): create a post using the form and relation

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Form\PostType;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +20,21 @@ class PostController extends AbstractController
         return $this->render('post/create.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route('/post/store', methods: 'POST', name: 'app_post_store')]
+    public function store(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(PostType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Post created!');
+        }
+
+        return $this->redirectToRoute('app_post_create');
     }
 }
--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -3,35 +3,25 @@
 namespace App\Form;
 
 use App\Entity\Post;
+use App\Entity\Category;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('category', ChoiceType::class, [
-                'choices' => [
-                    // 'PHP' => 'php',
-                    // 'Laravel' => 'laravel',
-                    // 'Symfony' => 'symfony'
-                    'Languages' => [
-                        'PHP' => 'php',
-                        'Javascript' => 'javascript',
-                    ],
-                    'Frameworks' => [
-                        'Laravel' => 'laravel',
-                        'Symfony' => 'symfony',
-                        'React' => 'react',
-                        'Angular' => 'angular'
-                    ]
-                ],
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
                 'placeholder' => 'Select one...',
                 'label' => 'Categories'
             ])
